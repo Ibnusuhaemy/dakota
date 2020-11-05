26487c22ad44038157010f28a95f1b3590d193f5
ibnusuhaemy3 <ibnu@clloh.com>
2020-11-05 20:02:26 +0700

    feat: develop go to header link

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,19 +1,32 @@
 // place your custom js here
-$(document).ready(function(){
-  // Hero Slider
-  $('.js-hero-slider').slick({
-    dots: true,
-    slidesToShow: 1,
-    slidesToScroll: 1
-  });
-  
-  // Testimonial Slider
-  $('.js-testi-slider').slick();
-  
-  // Partnet Logo Slider
-  $('.js-partner-slider').slick({
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3
-  });
+
+// Hero Slider
+$('.js-hero-slider').slick({
+  dots: true,
+  slidesToShow: 1,
+  slidesToScroll: 1
+});
+
+// Testimonial Slider
+$('.js-testi-slider').slick();
+
+// Partner Logo Slider
+$('.js-partner-slider').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3
+});
+
+// Go Up Button
+$('.js-upper-link').on('click', function(event){
+  if (this.hash !== "") {
+    event.preventDefault();
+    var hash = this.hash;
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 800, function(){
+
+      window.location.hash = hash;
+    });
+  }
 });
