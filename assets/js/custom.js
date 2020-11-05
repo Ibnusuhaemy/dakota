8ab26a9b1cd51b08cb31b149fe365ed72f14384d
ibnusuhaemy3 <ibnu@clloh.com>
2020-11-05 19:47:49 +0700

    tweak: move custom js to appropriate file

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,1 +1,14 @@
 // place your custom js here
+$(document).ready(function(){
+  $('.js-hero-slider').slick({
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  });
+  $('.js-testi-slider').slick();
+  $('.js-partner-slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3
+  });
+});
