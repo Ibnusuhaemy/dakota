168396810518c81b2fb2c1d928bf47974dc9ba4b
ibnusuhaemy3 <ibnu@clloh.com>
2020-11-05 19:48:38 +0700

    tweak: adding comment on custom js code

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,11 +1,16 @@
 // place your custom js here
 $(document).ready(function(){
+  // Hero Slider
   $('.js-hero-slider').slick({
     dots: true,
     slidesToShow: 1,
     slidesToScroll: 1
   });
+  
+  // Testimonial Slider
   $('.js-testi-slider').slick();
+  
+  // Partnet Logo Slider
   $('.js-partner-slider').slick({
     infinite: true,
     slidesToShow: 3,
