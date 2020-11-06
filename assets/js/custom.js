41f45b76a364b3dadced7aed0a99647cdebda8f7
Ibnu Suhaemy <ibnu@clloh.com>
2020-11-06 15:01:10 +0700

    feature: mobile responsive, refactor some section, fix other issues

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -14,7 +14,19 @@ $('.js-testi-slider').slick();
 $('.js-partner-slider').slick({
   infinite: true,
   slidesToShow: 3,
-  slidesToScroll: 3
+  slidesToScroll: 3,
+  responsive: [
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
 });
 
 // Go Up Button
